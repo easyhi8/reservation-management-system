1f6ad2ddf15651b8cb491fb3b6eabb5aafc8b250
h.iijima <easy.s2m@gmail.com>
2025-05-02 11:43:00 +0900

    メニューバーの設定とボタンカラーの修正

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 // App.js
-
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -10,17 +9,48 @@ import Admin from "./pages/AdminPage";
 import ReservationForm from "./components/ReservationForm";
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const isLoginPage = location.pathname === "/" || location.pathname === "/register";
+  const isHomePage = location.pathname === "/home";
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/reservations" element={<Admin />} />
-        <Route path="/reservations/new" element={<ReservationForm />} />
-        <Route path="/reservations/edit/:id" element={<ReservationForm />} />
-      </Routes>
-    </div>
+    <>
+      {!isLoginPage && (
+        <nav className="navbar">
+          <ul className="nav-list">
+            <li onClick={() => navigate("/home")}>ホーム</li>
+            <li onClick={() => navigate("/reservations")}>予約一覧</li>
+            <li
+              onClick={() => {
+                localStorage.removeItem("token");
+                navigate("/");
+              }}
+            >
+              ログアウト
+            </li>
+          </ul>
+        </nav>
+      )}
+
+      {/* Homeページだけは枠なし表示 */}
+      {isHomePage ? (
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      ) : (
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/reservations" element={<Admin />} />
+            <Route path="/reservations/new" element={<ReservationForm />} />
+            <Route path="/reservations/edit/:id" element={<ReservationForm />} />
+          </Routes>
+        </div>
+      )}
+    </>
   );
 }
 
